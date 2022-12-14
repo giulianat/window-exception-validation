35833244120e1a16f690b548b0f0cbc4456078be
Giuliana Taylor <giuliana.taylor@imperfectproduce.com>
2022-12-14 15:39:43 -0500

    Organize Entities into their own directory

---
+++ WindowExceptionsGenerator/Entities/Zone.cs
@@ -0,0 +1,48 @@
+using CsvHelper.Configuration.Attributes;
+
+namespace WindowExceptionsGenerator;
+
+public abstract class LocalZone
+{
+    private string _pickupTime;
+    private string _isLineHaul;
+    [Name("zoneId"), Index(0)] public string ZoneId { get; set; }
+
+    [Name("fulfillmentCenterId"), Index(1)] public string FulfillmentCenterId { get; set; }
+
+    [Name("name"), Index(2)] public string Name { get; set; }
+
+    [Name("timezone"), Index(3)] public string Timezone { get; set; }
+
+    [Name("zonePickupAddressId"), Index(4)] public string ZonePickupAddressId { get; set; }
+
+    [Name("expectedServiceTimeInMinutes"), Index(5)] public string ExpectedServiceTimeInMinutes { get; set; }
+
+    [Name("isLineHaul"), Index(6)]
+    public string IsLineHaul
+    {
+        get => _isLineHaul.ToUpper();
+        set => _isLineHaul = value;
+    }
+
+    [Name("pickupTime"), Index(7)]
+    public string PickupTime
+    {
+        get => _pickupTime;
+        set => _pickupTime = TimeOnly.ParseExact(value, "H:mm:ss").ToString("HH:mm");
+    }
+
+    [Name("marketCode"), Index(8)] public string MarketCode { get; set; }
+}
+
+public class LineHaulZone : LocalZone
+{
+    private string _transitTime;
+
+    [Name("transitTime"), Index(9)]
+    public string TransitTime
+    {
+        get => _transitTime;
+        set => _transitTime = TimeOnly.ParseExact(value, "H:mm:ss").ToString("HH:mm");
+    }
+}
--- WindowExceptionsGenerator/Generator.cs
+++ WindowExceptionsGenerator/Generator.cs
@@ -8,13 +8,13 @@ namespace WindowExceptionsGenerator;
 
 public class Generator
 {
+    private readonly IEnumerable<CurrentDataRecord> _currentData;
     private readonly string _destinationPath;
     private readonly string _holiday;
-    private readonly IEnumerable<CurrentDataRecord> _currentData;
-    private readonly IEnumerable<OpsPlanRecord> _opsPlan;
     private readonly Dictionary<int, DateOnly> _holidayWeekMap;
     private readonly Dictionary<string, string> _marketCodeToNameMap;
     private readonly Dictionary<int, DayOfWeekMapRecord> _numericDayToDayOfWeekMap;
+    private readonly IEnumerable<OpsPlanRecord> _opsPlan;
 
     public Generator(string sourceDirectory, string destinationPath, string holiday, string sundayOfHolidayWeek)
     {
@@ -42,68 +42,74 @@ public class Generator
 
     public void GenerateZones()
     {
-        string FormatTimestamp(string time) => TimeOnly.ParseExact(time, "H:mm:ss").ToString("HH:mm");
+        Func<CurrentDataRecord, bool> ZoneNameMatches(OpsPlanRecord plan)
+        {
+            return d =>
+            {
+                var marketCode = plan.Zone_Code;
+                var day = _numericDayToDayOfWeekMap[plan.Exception_Delivery_Date].Day_Of_Week.ToUpper();
+                var zoneName = $"{marketCode}: {day}";
+                        
+                return d.Zone.Name.StartsWith(zoneName);
+            };
+        }
+
+        string FormatBooleanAsString(bool value)
+        {
+            return value.ToString().ToUpper();
+        }
+
         var doubleDeliveryZones = _opsPlan
+            .Where(plan => !plan.Is_Employee_Zone)
             .Where(plan =>
             {
-                var referenceZones = _currentData
-                    .Where(d =>
-                    {
-                        var zoneName =
-                            $"{plan.Zone_Code}: {_numericDayToDayOfWeekMap[plan.Exception_Delivery_Date].Day_Of_Week.ToUpper()}";
-                        return d.Zone.Name.StartsWith(zoneName);
-                    })
-                    .ToList();
+                var refWindowId = _currentData
+                    .FirstOrDefault(ZoneNameMatches(plan))
+                    ?.Window
+                    .WindowId;
 
-                return !plan.Is_Employee_Zone
-                       && referenceZones.Count == 1
-                       && _opsPlan.All(p => p.old_window_id != referenceZones.First().Window.WindowId);
+                return _currentData.Any(ZoneNameMatches(plan))
+                       && _opsPlan.All(p => p.old_window_id != refWindowId);
             });
 
-        var zonePairs = (
-            from zone in doubleDeliveryZones
-            let refZoneName =
-                $"{zone.Zone_Code}: {_numericDayToDayOfWeekMap[zone.Exception_Delivery_Date].Day_Of_Week.ToUpper()}"
+        var zonePairs = from zone in doubleDeliveryZones
+            let marketCode = zone.Zone_Code
+            let newDay = _numericDayToDayOfWeekMap[zone.Exception_Delivery_Date].Day_Of_Week.ToUpper()
+            let originalDay = _numericDayToDayOfWeekMap[zone.Original_Delivery_Date].Day_Of_Week.ToUpper()
+            let refZoneName = $"{marketCode}: {newDay}"
+            let movedZoneName = $"{marketCode}: {originalDay}"
             let referenceZone = _currentData.First(d => d.Zone.Name.StartsWith(refZoneName))
-            let movedZoneName =
-                $"{zone.Zone_Code}: {_numericDayToDayOfWeekMap[zone.Original_Delivery_Date].Day_Of_Week.ToUpper()}"
             let movedZone = _currentData.First(d => d.Zone.Name.StartsWith(movedZoneName))
-            select (movedZone, referenceZone)
-        ).ToList();
+            select (movedZone, referenceZone);
 
         var zones = (from @group in zonePairs
             let movedZone = @group.movedZone.Zone
-            let refZoneData = @group.referenceZone
-            let name = GenerateDoubleDeliveryZoneName(movedZone.Name, refZoneData.Window.StartDay)
-            select new
+            let refZone = @group.referenceZone.Zone
+            let refWindow = @group.referenceZone.Window
+            let name = GenerateDoubleDeliveryZoneName(movedZone.Name, refWindow.StartDay)
+            select new LineHaulZone
             {
-                zoneId = Guid.NewGuid().ToString(),
-                fulfillmentCenterId = refZoneData.Zone.FulfillmentCenterId,
-                name,
-                timezone = refZoneData.Zone.Timezone,
-                zonePickupAddressId = refZoneData.Zone.ZonePickupAddressId,
-                expectedServiceTimeInMinutes = refZoneData.Zone.ExpectedServiceTimeInMinutes,
-                isLineHaul = refZoneData.Zone.IsLineHaul.ToString().ToUpper(),
-                pickupTime = FormatTimestamp(refZoneData.Zone.PickupTime),
-                marketCode = refZoneData.Zone.MarketCode,
-                transitTime = FormatTimestamp(refZoneData.Zone.TransitTime)
+                ZoneId = Guid.NewGuid().ToString(),
+                FulfillmentCenterId = refZone.FulfillmentCenterId,
+                Name = name,
+                Timezone = refZone.Timezone,
+                ZonePickupAddressId = refZone.ZonePickupAddressId,
+                ExpectedServiceTimeInMinutes = refZone.ExpectedServiceTimeInMinutes,
+                IsLineHaul = refZone.IsLineHaul.ToString(),
+                PickupTime = refZone.PickupTime,
+                MarketCode = refZone.MarketCode,
+                TransitTime = refZone.TransitTime
             }).ToList();
 
-        var lineHaulZones = zones.Where(z => z.isLineHaul == "TRUE").ToList();
+        var lineHaulZones = zones
+            .Where(z => z.IsLineHaul == "TRUE")
+            .ToList();
         WriteToCsv("CSV Upload - Zones - Line Haul.csv", lineHaulZones);
 
-        var localZones = zones.Where(z => z.isLineHaul == "FALSE").Select(z => new
-        {
-            z.zoneId,
-            z.fulfillmentCenterId,
-            z.name,
-            z.timezone,
-            z.zonePickupAddressId,
-            z.expectedServiceTimeInMinutes,
-            z.isLineHaul,
-            z.pickupTime,
-            z.marketCode
-        }).ToList();
+        var localZones = zones
+            .Where(z => z.IsLineHaul == "FALSE")
+            .Cast<LocalZone>()
+            .ToList();
         WriteToCsv("CSV Upload - Zones - Local.csv", localZones);
     }
 
@@ -117,7 +123,7 @@ public class Generator
         return $"{prefix} / {refZoneDayOfWeek}{suffix}";
     }
 
-    private void WriteToCsv(string fileName, IEnumerable<object> data)
+    private void WriteToCsv<T>(string fileName, IEnumerable<T> data)
     {
         var path = $"{_destinationPath}/{fileName}";
         using var writer = File.CreateText(path);
